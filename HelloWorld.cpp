0bc92ba156367d23deb60d9d4b65193945f34eee
Miguel Ángel <147431597+miguesgo@users.noreply.github.com>
2023-10-24 09:25:49 -0600

    Hello World in CPP
    I have to start with something

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Hello World in C++
+    This piece of code just print out "Hello World"
+*/
+
+int main(){
+
+    cout<<"Hello world";
+    return 0;
+}
